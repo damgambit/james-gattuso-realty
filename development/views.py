816b5e564e6e9c88e7454c8017909f9b7a09e69a
damgambit <damianogambitta@gmail.com>
2017-11-26 14:27:05 +0100

    data import

--- development/views.py
+++ development/views.py
@@ -50,29 +50,97 @@ d1 = datetime.datetime.strptime('01/01/2001', '%m/%d/%Y')
 
 Timeline = namedtuple('Timeline', ('baystateauction', 'townauction'))
 
-# from models import *
-# import csv
+from models import *
+import csv
 
 
-# def parcer():
+def parcer():
+    f = open('./csv_file/towneauction.csv', 'r')
+    # Auction Date,Time,Status,Address,City,State,Zip Code,County,Page / Liber,Deposit
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] = 'Auction Date':
+            continue
+        c = TownAuction.objects.get_or_create(date=datetime.datetime.strptime(str(row[0]), '%m/%d/%y'),
+                                              time=row[1],
+                                              status=row[2],
+                                              address=row[3],
+                                              city=row[4],
+                                              state=row[5],
+                                              zipcode=row[6],
+                                              country=row[7],
+                                              deposit=row[9]
+                                              )
+    f.close()
 
 
-#     f = open('./csv_file/commonwealthauction.csv', 'r')
-#     # status,Auction_Date,Auction_time,Address,County,Deposit
-#     reader = csv.reader(f)
-#     for row in reader:
-#         if row[0] == 'status':
-#             continue
-#         c = CommonWealthAuction.objects.get_or_create(
-#               date=datetime.datetime.strftime(
-#                   datetime.datetime.strptime(row[1], "%m/%d/%Y"), "%Y-%m-%d"),
-#               time=row[2],
-#               status=row[0],
-#               address=row[3],
-#               country=row[4],
-#               deposit=row[5]
-#                                                       )
-#     f.close()
+    f = open('./csv_file/baystateauction.csv', 'r')
+    reader = csv.reader(f)
+    # Status,Day,Date,Time,Address,City,State,Deposit
+    for row in reader:
+        if row[0] == 'Status':
+            continue
+        c = BayStateAuction.objects.get_or_create(date=datetime.datetime.strptime(str(row[2]) + ' 2017', '%B %d %Y'),
+                                                  time=row[3],
+                                                  status=row[0],
+                                                  address=row[4],
+                                                  city=row[5],
+                                                  state=row[6],
+                                                  deposit=row[7]
+                                                  )
+    f.close()
+
+    f = open('./csv_file/pesco.csv', 'r')
+    #Date,Time,Title,Address,City,State,zipcode,Terms
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] == 'Date':
+            continue
+        c = Pesco.objects.get_or_create(date=row[0],
+                time=row[1],
+                status=row[2],
+                address=row[3],
+                city=row[4],
+                state=row[5],
+                zipcode=row[6],
+                terms=row[7]
+            )
+    f.close()
+    
+    f = open('./csv_file/landmarkauction.csv', 'r')
+    # Auction Date,Time,Status,Address,City,State,Zip Code
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] == 'Auction Date':
+            continue
+        c = LandMarkAuction.objects.get_or_create(date=row[0],
+                                                  time=row[1],
+                                                  status=row[2],
+                                                  address=row[3],
+                                                  city=row[4],
+                                                  state=row[5],
+                                                  zipcode=row[6],
+                                                  )
+    f.close()
+
+
+
+    f = open('./csv_file/commonwealthauction.csv', 'r')
+    # status,Auction_Date,Auction_time,Address,County,Deposit
+    reader = csv.reader(f)
+    for row in reader:
+        if row[0] == 'status':
+            continue
+        c = CommonWealthAuction.objects.get_or_create(
+              date=datetime.datetime.strftime(
+                  datetime.datetime.strptime(row[1], "%m/%d/%Y"), "%Y-%m-%d"),
+              time=row[2],
+              status=row[0],
+              address=row[3],
+              country=row[4],
+              deposit=row[5]
+                                                      )
+    f.close()
 
 
 
@@ -102,8 +170,8 @@ def login_view(request):
             'form': form,
             'name': "Login"
         }
-    # print('parcing')
-    # parcer()
+    print('parcing')
+    parcer()
     return render(request, 'login.html', context)
 
 
